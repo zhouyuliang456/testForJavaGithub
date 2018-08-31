077c7baa1f3e85ac58c1138fed0e4cc1f326010d
zhouyuliang456 <17612581229@163.com>
2018-08-31 23:04:13 +0800

    zyl add new code

--- com/company/Main.java
+++ com/company/Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hello world");
+        String s = "dsfdsf" ;
+        s+="342";
+        System.out.println(s);
     }
 }
