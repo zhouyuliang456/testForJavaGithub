101201cf51dd59a665c7af3f2f4fc0dd510e0285
zhouyuliang456 <17612581229@163.com>
2018-12-12 21:41:52 +0800

    新加文件

---
+++ com/company/Main1.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hello world");
+        String s = "dsfdsf" ;
+        s+="342";
+        System.out.println(s);
+    // test a new modified
+
+    // 新家文件1
+    
+    }
+}
